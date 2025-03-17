c5c7ae1f9314410a79eb48dc4c5b5b883c936c8e
MH-Rohan-JU <rohan70302@gmail.com>
2025-03-17 18:35:37 +0600

    Dark Mode Added

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const btn1 = document.querySelector('.btnimg');
 const btn2 = document.querySelector('.btnimg-2');
 const btn3 = document.querySelector('.btnimg-3');
 const btn4 = document.querySelector('.btnimg-4');
+const darkMode = document.querySelector('.dark-mode');
+const text = document.querySelectorAll('.text');
 
 btn1.addEventListener('click', () => {
     images1.forEach((imgs) => {
@@ -29,4 +31,14 @@ btn3.addEventListener('click', () => {
 btn4.addEventListener('click', () => {
     document.querySelector('.cse-4').classList.remove('hidden');
     btn4.classList.add('hidden');
+});
+
+darkMode.addEventListener('click', () => {
+    document.querySelector('.body').classList.toggle('bg-dark');
+    text.forEach((t) => {
+        t.classList.toggle('white');
+    });
+    document.querySelector('.texten').classList.toggle('white');
+    document.querySelector('.jucse').classList.toggle('white');
+    document.querySelector('.intro').classList.toggle('aqua');
 });
